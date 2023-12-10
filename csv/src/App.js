4e700e964e4bfe29d326e498fe3f800f60804286
Dakvalion <dnikitina26@gmail.com>
2023-12-11 00:04:16 +0300

    init: start task

---
+++ csv/src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div class='container'>
+      <div class='wrap'>
+        <p class='text'>Выберите файл в формате CSV</p>
+        <button class='btn'>Выберите файл</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
