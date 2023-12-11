2a67d9348a975da8b46cb352c1f630bd8e6eccd5
Dakvalion <dnikitina26@gmail.com>
2023-12-11 14:39:16 +0300

    feat: add localstorage methods

--- csv/src/components/Table.jsx
+++ csv/src/components/Table.jsx
@@ -1,7 +1,11 @@
 import { useNavigate } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import { usePapaParse } from 'react-papaparse';
 
 
-const headers = [
+
+const head = [
     {
         id:1,
         header:"Имя",
@@ -25,7 +29,7 @@ const headers = [
     {
         id:5,
         header: "Адрес",
-        width:"500px"
+        width:"380px"
     },
     
     
@@ -33,27 +37,67 @@ const headers = [
 
 export function Table () {
 
+    const location = useLocation();
+    const uploaded_file = location.state.file;
     const navigate = useNavigate();
+    const [tableData, setTableData] = useState(() => {
+        const savedData = localStorage.getItem('tableData');
+        return savedData ? JSON.parse(savedData) : null;
+    });
+    const { readString } = usePapaParse();
+
+    
+    
+    const parseCSV = (text) => {
+        readString(text, {
+         skipEmptyLines: true,
+         complete: (results) => {
+           results.data.splice(0, 1);
+           setTableData(results.data);
+           localStorage.setItem('tableData', JSON.stringify(results.data));
+         },
+        });
+    };
+    
+    
+    useEffect(() => {
+        if (!tableData) {
+        const reader = new FileReader();
+        reader.onload = function (event) {
+          parseCSV(event.target.result);
+        };
+        reader.readAsText(uploaded_file, 'CP1251');
+        }
+    }, [uploaded_file, tableData]);
+
+     
 
     const redirect = (event) => {
+        localStorage.clear();
         navigate("/");
     }
 
     return (
         <>
-        <button for='file_upload' class="upload" onClick={redirect}>Загрузить новый файл</button>
-        <input type="file" id="file_upload"/>
+        <button class="upload" onClick={redirect}>Загрузить новый файл</button>
         <table className="table_container" width="1670px">
             <thead>
                 <tr>
-                    {headers.map((item) =>{
+                    {head.map((item) =>{
                         return(
-                            <th width={item.width}>{item.header}</th>
+                            <th width={item.width} key={item.id}>{item.header}</th>
                         )
                     }
                     )}
                 </tr>
             </thead>
+                {tableData && tableData.map((row, rowIndex) => (
+                    <tr key={rowIndex}>
+                    {row.map((cell, cellIndex) => (
+                        <td key={cellIndex}>{cell}</td>
+                    ))}
+                    </tr>
+                ))}
         </table>
         </>
         
